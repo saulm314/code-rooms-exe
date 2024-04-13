41e8fd4428213ac60afe8bf690e6eb2203c23ab1
Saulius Markevicius <saulius0412@gmail.com>
2024-04-13 17:41:48 +0100

    Add basic Celsius-Fahrenheit converter
    This has nothing to do with the project; this was merely me following the Avalonia tutorial to familiarise myself with the platform. This code will be removed later but keeping it for now as a reference.

--- CREAvaloniaApp/Views/MainView.axaml.cs
+++ CREAvaloniaApp/Views/MainView.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace CREAvaloniaApp.Views;
 
@@ -7,5 +9,29 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+    }
+
+    private void Calculate()
+    {
+        if (double.TryParse(celsius.Text, out double C))
+        {
+            double F = C * (9.0 / 5.0) + 32;
+            fahrenheit.Text = F.ToString("0.0");
+        }
+        else
+        {
+            celsius.Text = "0";
+            fahrenheit.Text = "0";
+        }
+    }
+
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {
+        Calculate();
+    }
+
+    public void CelsiusUpdated(object source, AvaloniaPropertyChangedEventArgs args)
+    {
+        Calculate();
     }
 }
