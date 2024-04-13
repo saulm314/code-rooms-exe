6929734eef71c48f46e5ea4136b7282752c71946
Saulius Markevicius <saulius0412@gmail.com>
2024-04-13 23:30:20 +0100

    Set heap grid background colour

--- CREAvaloniaApp/Views/MainView.axaml.cs
+++ CREAvaloniaApp/Views/MainView.axaml.cs
@@ -1,6 +1,5 @@
-﻿using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Interactivity;
+﻿using Avalonia.Controls;
+using Avalonia.Media;
 
 namespace CREAvaloniaApp.Views;
 
@@ -9,5 +8,17 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
+        SetHeapGridBackground();
+    }
+
+    private const byte HEAP_GRID_BG_A = byte.MaxValue;
+    private const byte HEAP_GRID_BG_R = 100;
+    private const byte HEAP_GRID_BG_G = 100;
+    private const byte HEAP_GRID_BG_B = 100;
+    private void SetHeapGridBackground()
+    {
+        Color backgroundColor = new(HEAP_GRID_BG_A, HEAP_GRID_BG_R, HEAP_GRID_BG_G, HEAP_GRID_BG_B);
+        SolidColorBrush brush = new(backgroundColor);
+        heapGrid.Background = brush;
     }
 }
