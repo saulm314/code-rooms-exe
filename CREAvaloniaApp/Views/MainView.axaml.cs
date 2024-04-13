7a0b58ad2cc835546ba03ef68e9f2d783f5400db
Saulius Markevicius <saulius0412@gmail.com>
2024-04-13 21:59:51 +0100

    Remove temperature converter

--- CREAvaloniaApp/Views/MainView.axaml.cs
+++ CREAvaloniaApp/Views/MainView.axaml.cs
@@ -10,28 +10,4 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
     }
-
-    private void Calculate()
-    {
-        if (double.TryParse(celsius.Text, out double C))
-        {
-            double F = C * (9.0 / 5.0) + 32;
-            fahrenheit.Text = F.ToString("0.0");
-        }
-        else
-        {
-            celsius.Text = "0";
-            fahrenheit.Text = "0";
-        }
-    }
-
-    public void ButtonClicked(object source, RoutedEventArgs args)
-    {
-        Calculate();
-    }
-
-    public void CelsiusUpdated(object source, AvaloniaPropertyChangedEventArgs args)
-    {
-        Calculate();
-    }
 }
