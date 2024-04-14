2d94d52d07d9bf5b2063dbfc2fb02a35a5a23b03
Saulius Markevicius <saulius0412@gmail.com>
2024-04-14 22:21:14 +0100

    Add automatic newline tabbing

--- CREAvaloniaApp/Views/MainView.axaml.cs
+++ CREAvaloniaApp/Views/MainView.axaml.cs
@@ -1,5 +1,7 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Media;
+using System.Collections.Generic;
 
 namespace CREAvaloniaApp.Views;
 
@@ -17,5 +19,28 @@ public partial class MainView : UserControl
         Color color = new(byte.MaxValue, STACK_HEADER_BG, STACK_HEADER_BG, STACK_HEADER_BG);
         SolidColorBrush brush = new(color);
         stackHeaderTextBlock.Background = brush;
+    }
+
+    public void OnTextChange(object sender, AvaloniaPropertyChangedEventArgs e)
+    {
+        int caretIndex = textEditorTextBox.CaretIndex;
+        if (caretIndex < 1)
+            return;
+        if (textEditorTextBox.Text == null)
+            return;
+        int previousNewlineIndex = textEditorTextBox.Text.LastIndexOf('\n', caretIndex - 1);
+        if (previousNewlineIndex == -1)
+            return;
+        int tabCount = 0;
+        int i = previousNewlineIndex + 1;
+        while (i < textEditorTextBox.Text.Length && textEditorTextBox.Text[i] == '\t')
+        {
+            tabCount++;
+            i++;
+        }
+        List<char> newlineChars = new() { '\n' };
+        for (int j = 0; j < tabCount; j++)
+            newlineChars.Add('\t');
+        textEditorTextBox.NewLine = new(newlineChars.ToArray());
     }
 }
