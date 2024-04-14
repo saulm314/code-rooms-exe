9959eca644a6b5a48a9fbc005a19cb7abb765397
Saulius Markevicius <saulius0412@gmail.com>
2024-04-14 03:34:19 +0100

    Add heap indexes and change heap colours

--- CREAvaloniaApp/Views/MainView.axaml.cs
+++ CREAvaloniaApp/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Layout;
 using Avalonia.Media;
 
 namespace CREAvaloniaApp.Views;
@@ -10,9 +11,10 @@ public partial class MainView : UserControl
         InitializeComponent();
         SetHeapGridBackground();
         SetHeapGridForeground();
+        SetHeapIndexes();
     }
 
-    public const byte HEAP_GRID_BG = 100;
+    public const byte HEAP_GRID_BG = 0;
     private void SetHeapGridBackground()
     {
         Color backgroundColor = new(byte.MaxValue, HEAP_GRID_BG, HEAP_GRID_BG, HEAP_GRID_BG);
@@ -34,13 +36,45 @@ public partial class MainView : UserControl
         for (int i = 1; i < rowDefinitions.Count; i += 2)
             for (int j = 1; j < columnDefinitions.Count; j += 2)
             {
-                Panel panel = new();
-                panel[Grid.RowProperty] = i;
-                panel[Grid.ColumnProperty] = j;
-                panel.Background = brush;
+                Panel panel = new()
+                {
+                    Background = brush,
+                    [Grid.RowProperty] = i,
+                    [Grid.ColumnProperty] = j
+                };
                 heapGrid.Children.Add(panel);
                 HeapPanels[panelsAdded] = panel;
                 panelsAdded++;
+            }
+    }
+
+    public TextBlock[] HeapIndexTextBlocks { get; } = new TextBlock[HEAP_PAGE_SIZE];
+
+    public const byte HEAP_INDEX_FG = HEAP_GRID_BG;
+    private void SetHeapIndexes()
+    {
+        Color foregroundColor = new(byte.MaxValue, 150, 150, 150);
+        SolidColorBrush foregroundBrush = new(foregroundColor);
+        RowDefinitions rowDefinitions = heapGrid.RowDefinitions;
+        ColumnDefinitions columnDefinitions = heapGrid.ColumnDefinitions;
+        int count = 0;
+        for (int i = 2; i < rowDefinitions.Count; i += 2)
+            for (int j = 1; j < columnDefinitions.Count; j += 2)
+            {
+                TextBlock textBlock = new()
+                {
+                    Text = count == 0 ? "null" : count.ToString(),
+                    FontSize = 13,
+                    FontFamily = new("Cascadia Mono"),
+                    Foreground = foregroundBrush,
+                    FontWeight = FontWeight.UltraBold,
+                    TextAlignment = TextAlignment.Center,
+                    [Grid.RowProperty] = i,
+                    [Grid.ColumnProperty] = j
+                };
+                heapGrid.Children.Add(textBlock);
+                HeapIndexTextBlocks[count] = textBlock;
+                count++;
             }
     }
 }
