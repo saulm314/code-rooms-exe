f571dbb75090ff715d0f445b920557d9749f6661
Saulius Markevicius <saulius0412@gmail.com>
2024-04-14 17:20:07 +0100

    Add scroll viewer to stack

--- CREAvaloniaApp/Views/MainView.axaml.cs
+++ CREAvaloniaApp/Views/MainView.axaml.cs
@@ -8,11 +8,17 @@ public partial class MainView : UserControl
     {
         InitializeComponent();
         SetResolution();
+        SetStackHeight();
     }
 
     private void SetResolution()
     {
         Width = Properties.Instance.Resolution.x;
         Height = Properties.Instance.Resolution.y;
+    }
+
+    private void SetStackHeight()
+    {
+        stackScrollViewer.Height = Height - stackHeaderTextBlock.Height - stackHeaderPanel.Height;
     }
 }
