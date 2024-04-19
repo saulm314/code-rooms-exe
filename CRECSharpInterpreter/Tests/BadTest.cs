a477061b6939f2d365b2bde090049656bedae2c6
Saulius Markevicius <saulius0412@gmail.com>
2024-04-19 23:36:45 +0100

    Remove all tests

---
+++ CRECSharpInterpreter/Tests/BadTest.cs
@@ -0,0 +1,22 @@
+﻿using static CRECSharpInterpreter.VarType;
+using System;
+
+namespace CRECSharpInterpreter.Tests.Variables
+{
+    public class BadTest : ITest
+    {
+        public BadTest(string path, Error error)
+        {
+            Path = path;
+            Error = error;
+        }
+
+        public string Path { get; init; }
+
+        public Variable[][] Stack => Array.Empty<Variable[]>();
+
+        public Variable[] Heap => Array.Empty<Variable>();
+
+        public Error Error { get; init; }
+    }
+}
--- CRECSharpInterpreter/Tests/TestRepository.cs
+++ CRECSharpInterpreter/Tests/TestRepository.cs
@@ -1,55 +1,9 @@
-﻿using CRECSharpInterpreter.Tests.Variables;
-using CRECSharpInterpreter.Tests.ValueTypes;
-using CRECSharpInterpreter.Tests.ReferenceTypes.Creation.Arrays;
-using CRECSharpInterpreter.Tests.ReferenceTypes.Indexing.Arrays;
-
-namespace CRECSharpInterpreter.Tests
+﻿namespace CRECSharpInterpreter.Tests
 {
     public static class TestRepository
     {
         public static ITest[] Tests { get; } = new ITest[]
         {
-            new VarDeclaration(),
-            new BadVarDeclarationType(),
-            new BadVarDeclarationKeyword(),
-            new VarInitialisation(),
-            new BadVarInitialisationWrongType(),
-            new ReadWriteVar(),
-            new BadReadVarNotDeclared(),
-            new BadReadVarNotInitialised(),
-            new Int(),
-            new BadIntDouble(),
-            new BadIntOutOfBounds(),
-            new Bool(),
-            new Double(),
-            new BadDoubleInt(),
-            new Char(),
-            new BadCharEmpty(),
-            new BadCharInvalidEscapeSequence(),
-            new BadCharNewline(),
-            new BadCharNewlineChar(),
-            new BadCharSingleBackslash(),
-            new BadCharQuoteNotClosed(),
-            new BadCharQuoteNotOpened(),
-            new BadCharTwoChars(),
-            new ValueTypeArrayDeclInit(),
-            new ValueTypeArrayConstruction(),
-            new BadArrayConstructionNegative(),
-            new BadArrayConstructionNotInteger(),
-            new BadArrayConstructionNoArg(),
-            new ValueTypeArrayLiteral(),
-            new BadArrayLiteralWrongType(),
-            new ValueTypeArrayElementReadWrite(),
-            new BadArrayElementReadOutOfBounds(),
-            new BadArrayElementReadNegative(),
-            new BadArrayElementReadEmptyArr(),
-            new BadArrayElementReadNoArg(),
-            new BadArrayElementReadNotInteger(),
-            new BadArrayElementWriteOutOfBounds(),
-            new BadArrayElementWriteNegative(),
-            new BadArrayElementWriteEmptyArr(),
-            new BadArrayElementWriteNoArg(),
-            new BadArrayElementWriteNotInteger()
         };
 
         public static int SuccessfulTests { get; set; } = 0;
