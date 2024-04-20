57c8a9dfecaf7248dbd277c61e47c9eed55acdea
Saulius Markevicius <saulius0412@gmail.com>
2024-04-20 01:22:56 +0100

    Add ReferenceTypes\Indexing\Arrays test files

--- Files/Tests/ReferenceTypes/Creation/Arrays/StringArrayDeclInit.cs
+++ Files/Tests/ReferenceTypes/Creation/Arrays/StringArrayDeclInit.cs
@@ -2,4 +2,4 @@ string [ ] strArr ;
 string [ ] strArr2 = null ;
 string [ ] strArr3 = new string [ 0 ] ;
 string [ ] strArr4 = new string [ 2 ] ;
-string [ ] strArr5 = new string [ ] { "" , "a" , "ab" } ;
+string [ ] strArr5 = new string [ ] { null , "" , "a" , "ab" } ;
---
+++ Files/Tests/ReferenceTypes/Indexing/Arrays/BadCArrayElementWriteBraceNotOpened.cs
@@ -0,0 +1,2 @@
+int intArr = new int[3];
+intArr 1] = 0;
