cc1fb28c96d407ba2b1019f2d0dc3981c600b66f
Saulius Markevicius <saulius0412@gmail.com>
2024-04-20 03:57:26 +0100

    Add IfStatements test files

---
+++ Files/Tests/IfStatements/MultiIf.cs
@@ -0,0 +1,18 @@
+int myInt = 0 ;
+int myInt2 = 0 ;
+int myInt3 = 0 ;
+int myInt4 = 0 ;
+
+if ( true )
+{
+    myInt = 1 ;
+    myInt2 = 1 ;
+}
+
+if ( false )
+{
+    myInt3 = 1 ;
+    myInt4 = 1 ;
+}
+
+if ( true ) { }
