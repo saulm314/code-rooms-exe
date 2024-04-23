781a5d4b078c7abf6dd0b150241aa74ad0f8f41b
Saulius Markevicius <saulius0412@gmail.com>
2024-04-23 21:10:40 +0100

    Add SingleIfMultiElse test

---
+++ CRECSharpInterpreter/Tests/Good/SingleIfMultiElse.cs
@@ -0,0 +1,34 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class SingleIfMultiElse : ITest
+    {
+        public SingleIfMultiElse(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@int, "myInt", 1, true),
+                    new(@int, "myInt2", 0, true),
+                    new(@int, "myInt3", 2, true),
+                    new(@int, "myInt4", 2, true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
