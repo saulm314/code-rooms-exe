256b2e78a3a57bdabe443af8195b32607cd2c8ab
Saulius Markevicius <saulius0412@gmail.com>
2024-04-23 07:47:02 +0100

    Add C# -> Java test converter
    This is a C# program that finds each .cs test file, makes a copy of it, renames it to a .java file, then replaces all "bool" with "boolean", all "string" with "String", and all "Length" with "length". Finally, it prints out which files contain "length", so that we can manually add brackets "()" for string lengths.

---
+++ CRETestCsJavaConverter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace CRETestCsJavaConverter
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Copying and converting .cs files to .java...");
+            string directory = @"..\..\..\..\Files\Tests";
+            ConvertDirectoriesToJava(new string[] { directory });
+            Console.WriteLine("Done");
+        }
+
+        private static void ConvertDirectoriesToJava(string[] directories)
+        {
+            foreach (string directory in directories)
+            {
+                string[] files = Directory.GetFiles(directory);
+                ConvertFilesToJava(files);
+                string[] subdirectories = Directory.GetDirectories(directory);
+                ConvertDirectoriesToJava(subdirectories);
+            }
+        }
+
+        private static void ConvertFilesToJava(string[] files)
+        {
+            string[] csFiles = files.Select(file => file).Where(file => file.EndsWith(".cs")).ToArray();
+            foreach (string csFile in csFiles)
+            {
+                string javaFile = csFile[0..(csFile.Length - 2)] + "java";
+                File.Copy(csFile, javaFile, true);
+                ConvertJavaFile(javaFile);
+            }
+        }
+
+        private static void ConvertJavaFile(string javaFile)
+        {
+            string contents = File.ReadAllText(javaFile);
+            contents = contents.Replace("bool", "boolean");
+            contents = contents.Replace("string", "String");
+            contents = contents.Replace("Length", "length");
+            if (contents.Contains("length"))
+                Console.WriteLine($"Contains \"length\": {javaFile}");
+            File.WriteAllText(javaFile, contents);
+        }
+    }
+}
