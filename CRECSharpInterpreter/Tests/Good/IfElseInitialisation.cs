adb60f60c799b39569a2a225200a27554919b764
Saulius Markevicius <saulius0412@gmail.com>
2024-04-23 21:15:49 +0100

    Add IfElseInitialisation test

---
+++ CRECSharpInterpreter/Tests/Good/IfElseInitialisation.cs
@@ -0,0 +1,32 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class IfElseInitialisation : ITest
+    {
+        public IfElseInitialisation(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@int, "myInt", 1, true),
+                    new(@int, "myInt2", 1, true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
