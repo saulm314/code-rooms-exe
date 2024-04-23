cef32c58443c9e61b47067710497bc8128018b34
Saulius Markevicius <saulius0412@gmail.com>
2024-04-23 21:15:02 +0100

    Add MultiIf test

---
+++ CRECSharpInterpreter/Tests/Good/MultiIf.cs
@@ -0,0 +1,34 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class MultiIf : ITest
+    {
+        public MultiIf(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@int, "myInt", 1, true),
+                    new(@int, "myInt2", 1, true),
+                    new(@int, "myInt3", 0, true),
+                    new(@int, "myInt4", 0, true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
