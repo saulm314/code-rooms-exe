639cff1c5c3f9d28cb0ff5fb6ce2c2d3eef595f5
Saulius Markevicius <saulius0412@gmail.com>
2024-04-23 19:30:08 +0100

    Add Multiplication test

---
+++ CRECSharpInterpreter/Tests/Good/Multiplication.cs
@@ -0,0 +1,32 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class Multiplication : ITest
+    {
+        public Multiplication(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@int, "myInt", 6, true),
+                    new(@double, "myDouble", 6.0, true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
