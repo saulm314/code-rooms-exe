7ab508909b83ee6d6fc24f98c7ea5db8944cfdf1
Saulius Markevicius <saulius0412@gmail.com>
2024-04-24 20:32:15 +0100

    Add SingleFor test

---
+++ CRECSharpInterpreter/Tests/Good/SingleFor.cs
@@ -0,0 +1,32 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class SingleFor : ITest
+    {
+        public SingleFor(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@int, "myInt", 3, true),
+                    new(@int, "myInt2", 0, true),
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
