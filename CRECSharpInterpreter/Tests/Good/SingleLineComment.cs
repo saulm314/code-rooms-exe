3a6954f5f88de2ef05e4cc26be3e9114ecaeeb58
Saulius Markevicius <saulius0412@gmail.com>
2024-04-24 20:58:32 +0100

    Add SingleLineComment test

---
+++ CRECSharpInterpreter/Tests/Good/SingleLineComment.cs
@@ -0,0 +1,31 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class SingleLineComment : ITest
+    {
+        public SingleLineComment(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@int, "i", 0, true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
