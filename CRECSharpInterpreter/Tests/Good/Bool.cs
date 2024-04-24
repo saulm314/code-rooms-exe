9dd8ffb1df912a3707338f6f7559daef9b2215cd
Saulius Markevicius <saulius0412@gmail.com>
2024-04-25 00:06:43 +0100

    Add Bool test

---
+++ CRECSharpInterpreter/Tests/Good/Bool.cs
@@ -0,0 +1,33 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class Bool : ITest
+    {
+        public Bool(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@bool, "myB", false, false),
+                    new(@bool, "myB2", false, true),
+                    new(@bool, "myB3", true, true)
+                }
+            };
+
+        public Variable?[] Heap =>
+            new Variable?[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
