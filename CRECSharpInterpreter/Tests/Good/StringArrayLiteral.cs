b9017381582ae7d1a387ad1a830670d32a668db7
Saulius Markevicius <saulius0412@gmail.com>
2024-04-24 23:21:00 +0100

    Update StringArrayLiteral test

--- CRECSharpInterpreter/Tests/Good/StringArrayLiteral.cs
+++ CRECSharpInterpreter/Tests/Good/StringArrayLiteral.cs
@@ -18,7 +18,9 @@ namespace CRECSharpInterpreter.Tests.Good
                 {
                     new(@string.Array, "strArr", 1, true),
                     new(@string.Array, "strArr2", 3, true),
-                    new(@string.Array, "strArr3", 8, true)
+                    new(@string.Array, "strArr3", 8, true),
+                    new(@char.Array, "charArr", 11, true),
+                    new(@string.Array, "strArr4", 29, true)
                 }
             };
 
@@ -35,7 +37,30 @@ namespace CRECSharpInterpreter.Tests.Good
                 new(@char, 'a'),
                 new(@int, 2),
                 new(@string, 5),
-                new(@string, 6)
+                new(@string, 6),
+                new(@int, 3),
+                new(@char, 'x'),
+                new(@char, 'y'),
+                new(@char, 'z'),
+                new(@int, 2),
+                new(@char, '\0'),
+                new(@char, '\0'),
+                new(@int, 2),
+                new(@char, 'a'),
+                new(@char, 'b'),
+                new(@int, 3),
+                new(@char, 'x'),
+                new(@char, 'y'),
+                new(@char, 'z'),
+                new(@int, 3),
+                new(@char, 'p'),
+                new(@char, 'q'),
+                new(@char, 'r'),
+                new(@int, 4),
+                new(@string, 15),
+                new(@string, 18),
+                new(@string, 21),
+                new(@string, 25)
             };
 
         public Error Error { get; } = Error.None;
--- Files/Tests/ReferenceTypes/Creation/Arrays/StringArrayLiteral.cs
+++ Files/Tests/ReferenceTypes/Creation/Arrays/StringArrayLiteral.cs
@@ -1,3 +1,11 @@
 string [ ] strArr = new string [ ] { } ;
 string [ ] strArr2 = new string [ ] { "" } ;
 string [ ] strArr3 = new string [ ] { "" , "a" } ;
+char [ ] charArr = new char [ ] { 'x' , 'y' , 'z' } ;
+string [ ] strArr4 = new string [ ]
+{
+    new string ( new char [ 2 ] ) ,
+    new string ( new char [ ] { 'a' , 'b' } ) ,
+    new string ( charArr ) ,
+    "pqr"
+} ;
