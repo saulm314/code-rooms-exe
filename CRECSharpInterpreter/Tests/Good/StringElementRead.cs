ca78eb84a98a3d5984d23e49504e23f20b0f701c
Saulius Markevicius <saulius0412@gmail.com>
2024-04-24 21:56:12 +0100

    Add StringElementRead test

---
+++ CRECSharpInterpreter/Tests/Good/StringElementRead.cs
@@ -0,0 +1,38 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class StringElementRead : ITest
+    {
+        public StringElementRead(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@string, "myStr", 1, true),
+                    new(@char, "myChar", 'a', true),
+                    new(@char, "myChar2", 'b', true),
+                    new(@char, "myChar3", 'c', true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null),
+                new(@int, 3),
+                new(@char, 'a'),
+                new(@char, 'b'),
+                new(@char, 'c')
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
