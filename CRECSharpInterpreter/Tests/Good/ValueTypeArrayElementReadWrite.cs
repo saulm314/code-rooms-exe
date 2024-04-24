655b63cd68454282122df53007ab3b64c98f32b6
Saulius Markevicius <saulius0412@gmail.com>
2024-04-24 21:58:57 +0100

    Add ValueTypeArrayElementReadWrite test

---
+++ CRECSharpInterpreter/Tests/Good/ValueTypeArrayElementReadWrite.cs
@@ -0,0 +1,43 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class ValueTypeArrayElementReadWrite : ITest
+    {
+        public ValueTypeArrayElementReadWrite(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@int.Array, "intArr", 1, true),
+                    new(@int, "myInt", 3, true),
+                    new(@int, "myInt2", 6, true),
+                    new(@int, "myInt3", 9, true),
+                    new(@int.Array, "reverseIntArr", 5, true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null),
+                new(@int, 3),
+                new(@int, 3),
+                new(@int, 6),
+                new(@int, 9),
+                new(@int, 3),
+                new(@int, 9),
+                new(@int, 6),
+                new(@int, 3)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
