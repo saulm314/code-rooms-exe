62b26db84d2dde401f2867a1a2154bebc253b6f6
Saulius Markevicius <saulius0412@gmail.com>
2024-04-24 20:29:00 +0100

    Add VarNamesIncludeKeywords tests

---
+++ Files/Tests/Misc/VarNamesIncludeKeywords.cs
@@ -0,0 +1,18 @@
+int if_ = 0 ;
+if_ = 1 ;
+
+int else_ = 0 ;
+if ( true ) ;
+else_ = 1 ;
+
+int while_ = 0 ;
+while_ = 1 ;
+
+int for_ = 0 ;
+for_ = 1 ;
+
+int break_ = 0 ;
+break_ = 1 ;
+
+int continue_ = 0 ;
+continue_ = 1 ;
