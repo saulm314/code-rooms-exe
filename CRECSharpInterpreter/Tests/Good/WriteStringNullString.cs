9f29cd11eac1c1485cc7820c09f7648bf1bfe3a4
Saulius Markevicius <saulius0412@gmail.com>
2024-04-24 23:22:14 +0100

    Add WriteStringNullString test

---
+++ CRECSharpInterpreter/Tests/Good/WriteStringNullString.cs
@@ -0,0 +1,32 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class WriteStringNullString : ITest
+    {
+        public WriteStringNullString(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@string, "myStr", null, true),
+                    new(@string, "myStr2", null, true)
+                }
+            };
+
+        public Variable[] Heap =>
+            new Variable[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
