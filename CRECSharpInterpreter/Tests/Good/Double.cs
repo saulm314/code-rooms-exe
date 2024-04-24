e2debcfc83db940311e9894c3b9e3d9c0ce6eb12
Saulius Markevicius <saulius0412@gmail.com>
2024-04-25 00:01:57 +0100

    Add Double test and fix divide by zero exception
    Turns out in C# you can actually divide a double by zero, but for the sake of this interpreter it is acceptable to simply throw an error

---
+++ CRECSharpInterpreter/Tests/Good/Double.cs
@@ -0,0 +1,34 @@
+﻿using static CRECSharpInterpreter.VarType;
+
+namespace CRECSharpInterpreter.Tests.Good
+{
+    public class Double : ITest
+    {
+        public Double(string pathNoExt)
+        {
+            PathNoExt = pathNoExt;
+        }
+
+        public string PathNoExt { get; init; }
+
+        public Variable[][] Stack => 
+            new[]
+            {
+                new Variable[]
+                {
+                    new(@double, "myDouble", 0.0, false),
+                    new(@double, "myDouble2", 0.0, true),
+                    new(@double, "myDouble3", -15.0, true),
+                    new(@double, "myDouble4", 3.14, true),
+                }
+            };
+
+        public Variable?[] Heap =>
+            new Variable?[]
+            {
+                new(null)
+            };
+
+        public Error Error { get; } = Error.None;
+    }
+}
