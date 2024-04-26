88282731a5bd993ff2d51c95486a8d2833bdd1e4
Saulius Markevicius <saulius0412@gmail.com>
2024-04-26 21:09:24 +0100

    Fix double -> int casts and amend tests

--- Files/Tests/Expressions/Operations/Misc/Cast.cs
+++ Files/Tests/Expressions/Operations/Misc/Cast.cs
@@ -17,7 +17,8 @@ string [ ] strArr3 = ( string [ ] ) null ;
 
 double myDouble2 = ( double ) 2 ;
 int myInt2 = ( int ) 2.0 ;
+int myInt3 = ( int ) 2.7 ;
 
-int myInt3 = ( int ) ( int ) 5 ;
-int myInt4 = ( int ) ( int ) ( int ) 5 ;
-int myInt5 = ( int ) ( double ) ( int ) 5.0 ;
+int myInt4 = ( int ) ( int ) 5 ;
+int myInt5 = ( int ) ( int ) ( int ) 5 ;
+int myInt6 = ( int ) ( double ) ( int ) 5.0 ;
