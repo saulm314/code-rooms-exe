8b0d9576918c99f4102ee8f19b0a64c1c59d7b9f
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-03 17:56:39 +0100

    Add syntax selection

--- CREInterpreter/Program.cs
+++ CREInterpreter/Program.cs
@@ -1,5 +1,6 @@
 ﻿namespace CREInterpreter;
 
+using System;
 using System.Diagnostics;
 
 public class Program
@@ -7,6 +8,32 @@ public class Program
     private static void Main(string[] args)
     {
         Trace.Listeners.Add(new ConsoleTraceListener());
-        Debug.WriteLine("Hello");
+        Debug.WriteLine("Running CREInterpreter in debug mode with args:");
+        foreach (string arg in args)
+            Debug.WriteLine($"\t{arg}");
+        Debug.WriteLine(null);
+
+        Console.WriteLine("Choose syntax: [1-2]");
+        Console.WriteLine("\t1) C#");
+        Console.WriteLine("\t2) Java");
+        Console.Write("> ");
+        string? choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                Environment.Instance._Syntax = Syntax.CSharp;
+                break;
+            case "2":
+                Environment.Instance._Syntax = Syntax.Java;
+                break;
+            default:
+                Console.Error.WriteLine($"Unrecognised choice {choice}, setting default value");
+                break;
+        }
+        Console.WriteLine($"Setting syntax to {Environment.Instance._Syntax}");
+        Console.WriteLine();
+        Console.ReadLine();
+
+
     }
 }
