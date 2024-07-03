22b1cac2cd4570b0e1ef5d47ae3603c6fa55aa95
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-03 15:19:56 +0100

    Add new CodeRoomsExe solution

---
+++ CREInterpreter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CREInterpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
