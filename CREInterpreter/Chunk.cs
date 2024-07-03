427e65545c740ee67e943ece4a94b99a5383d3c4
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-03 20:31:51 +0100

    Add Chunk class

---
+++ CREInterpreter/Chunk.cs
@@ -0,0 +1,16 @@
+﻿namespace CREInterpreter;
+
+using System.Collections.Generic;
+
+public class Chunk(string Text)
+{
+    public InterpreterException? Compile()
+    {
+        return null;
+    }
+
+    public IEnumerable<InterpreterException?> Run()
+    {
+        yield break;
+    }
+}
