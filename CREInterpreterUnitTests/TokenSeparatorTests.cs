580f6be1f1ffbae85034396c1d10a6c408d7dce5
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-03 23:40:19 +0100

    Add first test

---
+++ CREInterpreterUnitTests/TokenSeparatorTests.cs
@@ -0,0 +1,18 @@
+using CREInterpreter.Tokens;
+using System;
+using System.Linq;
+
+namespace CREInterpreterUnitTests;
+
+public class TokenSeparatorTests
+{
+    [Fact]
+    public void GetTokens_Null_ThrowsNullReferenceException()
+    {
+        string input = null!;
+
+        Action actual = () => TokenSeparator.GetTokens(input).ToArray();
+
+        Assert.Throws<NullReferenceException>(actual);
+    }
+}
