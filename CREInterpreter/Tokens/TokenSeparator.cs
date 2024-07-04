8712c473adc5a09486c9b0842fc0877a542e518d
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-04 15:18:11 +0100

    Add GetSingleLineComment method

--- CREInterpreter/Tokens/TokenSeparator.cs
+++ CREInterpreter/Tokens/TokenSeparator.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace CREInterpreter.Tokens;
 
@@ -6,6 +7,30 @@ public static class TokenSeparator
 {
     public static IEnumerable<IToken> GetTokens(string text)
     {
-        yield break;
+        int i = 0;
+        while (i < text.Length)
+        {
+
+        }
+    }
+
+    private static IToken? GetSingleLineCommentToken(string text, int startIndex, out int endIndex, ref int lineNumber)
+    {
+        endIndex = startIndex;
+        if (text.Length - startIndex < 2)
+            return null;
+        if (text[startIndex] != '/')
+            return null;
+        if (text[startIndex + 1] != '/')
+            return null;
+        StringBuilder commentBuilder = new("//");
+        int i = startIndex + 2;
+        while (i < text.Length && text[i] != '\n')
+            commentBuilder.Append(text[i++]);
+        endIndex = i;
+        SingleLineCommentToken token = new(text[startIndex..endIndex], lineNumber);
+        if (i < text.Length)
+            lineNumber++;
+        return token;
     }
 }
