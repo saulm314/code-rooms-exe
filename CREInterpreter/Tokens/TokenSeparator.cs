6b784a8541175edd226754fe2462f83c7a8df820
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-04 15:37:31 +0100

    Add GetMultiLineCommentToken method

--- CREInterpreter/Tokens/TokenSeparator.cs
+++ CREInterpreter/Tokens/TokenSeparator.cs
@@ -17,6 +17,8 @@ public static class TokenSeparator
     private static IToken? GetSingleLineCommentToken(string text, int startIndex, out int endIndex, ref int lineNumber)
     {
         endIndex = startIndex;
+        int originalLineNumber = lineNumber;
+        int lineNumberTemp = lineNumber;
         if (text.Length - startIndex < 2)
             return null;
         if (text[startIndex] != '/')
@@ -26,10 +28,50 @@ public static class TokenSeparator
         int i = startIndex + 2;
         while (i < text.Length && text[i] != '\n')
             i++;
-        endIndex = i;
-        SingleLineCommentToken token = new(text[startIndex..endIndex], lineNumber);
         if (i < text.Length)
-            lineNumber++;
-        return token;
+            lineNumberTemp++;
+        endIndex = i;
+        lineNumber = lineNumberTemp;
+        return new SingleLineCommentToken(text[startIndex..endIndex], originalLineNumber);
+    }
+
+    private static IToken? GetMultiLineCommentToken(string text, int startIndex, out int endIndex, ref int lineNumber)
+    {
+        endIndex = startIndex;
+        int originalLineNumber = lineNumber;
+        int lineNumberTemp = lineNumber;
+        if (text.Length - startIndex < 4)
+            return null;
+        if (text[startIndex] != '/')
+            return null;
+        if (text[startIndex + 1] != '*')
+            return null;
+        int i = startIndex + 2;
+        bool previousIsAsterisk = false;
+        bool closed = false;
+        while (i < text.Length)
+        {
+            if (text[i] == '*')
+            {
+                previousIsAsterisk = true;
+                i++;
+                continue;
+            }
+            if (text[i] == '/' && previousIsAsterisk)
+            {
+                i++;
+                closed = true;
+                break;
+            }
+            if (text[i] == '\n')
+                lineNumberTemp++;
+            previousIsAsterisk = false;
+            i++;
+        }
+        if (!closed)
+            return null;
+        endIndex = i;
+        lineNumber = lineNumberTemp;
+        return new MultiLineCommentToken(text[startIndex..endIndex], originalLineNumber);
     }
 }
