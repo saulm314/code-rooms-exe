719498456951c99d06021434b2351160e6316771
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-06 23:26:44 +0100

    Add DeclarationInitialisationStatement class

---
+++ CREInterpreter/Statements/DeclarationInitialisationStatement.cs
@@ -0,0 +1,28 @@
+﻿using CREInterpreter.Tokens;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class DeclarationInitialisationStatement(IToken[] tokens, string chunkText, VarType varType, string variableName, IToken[] expressionTokens)
+    : Statement
+{
+    public override IToken[] Tokens => tokens;
+
+    public override string ChunkText => chunkText;
+
+    public VarType _VarType => varType;
+
+    public string VariableName => variableName;
+
+    public IToken[] ExpressionTokens => expressionTokens;
+
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerable<InterpreterException?> Run(Memory memory)
+    {
+        throw new System.NotImplementedException();
+    }
+}
