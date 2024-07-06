32a250be73ea502f44f9866bceea0e8612478d9f
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-06 23:35:08 +0100

    Add WriteElementStatement class

---
+++ CREInterpreter/Statements/WriteElementStatement.cs
@@ -0,0 +1,28 @@
+﻿using CREInterpreter.Tokens;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class WriteElementStatement(IToken[] tokens, string chunkText, string variableName, IToken[] elementExpressionTokens, IToken[] expressionTokens)
+    : Statement
+{
+    public override IToken[] Tokens => tokens;
+
+    public override string ChunkText => chunkText;
+
+    public string VariableName => variableName;
+
+    public IToken[] ElementExpressionTokens => elementExpressionTokens;
+
+    public IToken[] ExpressionTokens => expressionTokens;
+
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerable<InterpreterException?> Run(Memory memory)
+    {
+        throw new System.NotImplementedException();
+    }
+}
