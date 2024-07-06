ee6940cf04d72642267d4ef71cf7aaefc4e5ff26
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-06 22:56:21 +0100

    Add first statement derivation

---
+++ CREInterpreter/Statements/EmptyStatement.cs
@@ -0,0 +1,21 @@
+﻿using CREInterpreter.Tokens;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class EmptyStatement(IToken[] tokens, string chunkText) : Statement
+{
+    public override IToken[] Tokens => tokens;
+
+    public override string ChunkText => chunkText;
+
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerable<InterpreterException?> Run(Memory memory)
+    {
+        throw new System.NotImplementedException();
+    }
+}
