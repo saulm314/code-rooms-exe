6241bbe7d29bd55a929555f4328d2a72c8e9efd6
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-06 16:25:24 +0100

    Add Memory class

--- CREInterpreter/Memory.cs
+++ CREInterpreter/Memory.cs
@@ -1,3 +1,58 @@
-﻿namespace CREInterpreter;
+﻿using System.Collections.Generic;
 
-public class Memory { }
+namespace CREInterpreter;
+
+public class Memory(int initialHeapCapacity)
+{
+    public Stack<Scope> Stack { get; } = new();
+    public Heap Heap { get; } = new(initialHeapCapacity);
+
+    public void PushToStack()
+    {
+        Stack.Push([]);
+    }
+
+    public void PopFromStack()
+    {
+        Scope scope = Stack.Pop();
+        foreach (Variable variable in scope)
+        {
+            if (variable._VarType!._Storage == Storage.Value)
+                continue;
+            if (variable.Value == null)
+                continue;
+            // is non-null reference type
+            int heapIndex = (int)variable.Value;
+            Heap.DecrementReferenceCounter(heapIndex);
+        }
+    }
+
+    public IEnumerable<Variable> GetDeclaredVariables()
+    {
+        foreach (Scope scope in Stack)
+            foreach (Variable variable in scope)
+                yield return variable;
+    }
+
+    public Variable? GetVariable(string name)
+    {
+        foreach (Variable variable in GetDeclaredVariables())
+            if (variable.Name == name)
+                return variable;
+        return null;
+    }
+
+    public bool IsDeclared(string variableName)
+    {
+        foreach (Variable variable in GetDeclaredVariables())
+            if (variable.Name == variableName)
+                return true;
+        return false;
+    }
+
+    public void AddToCurrentScope(Variable variable)
+    {
+        Scope scope = Stack.Peek();
+        scope.Add(variable);
+    }
+}
