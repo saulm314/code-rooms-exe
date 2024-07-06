434b8424ae3a74f6a8ca26091a59c75f94854beb
Saulius Markevicius <smsmvicious@gmail.com>
2024-07-06 14:38:27 +0100

    Add Variable class

---
+++ CREInterpreter/Variable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace CREInterpreter;
+
+public class Variable(VarType? varType, string? name = null)
+{
+    public VarType? _VarType { get; } = varType;
+    public string? Name { get; } = name;
+    public object? Value { get; set; } = varType?.DefaultValue;
+    public bool Initialised { get; set; }
+
+    public string FormattedValue => ValueFormatter.FormatValue(Value, _VarType);
+
+    public override string ToString() => string.Format("{0,10} {1,20} {2,10}",
+            _VarType?.ToString() ?? "*",
+            Name?.ToString() ?? "*",
+            string.IsNullOrEmpty(FormattedValue) ? "*" : FormattedValue);
+
+    public static IEnumerable<Variable> GetBlankVariables(VarType varType, int count)
+    {
+        for (int i = 0; i < count; i++)
+            yield return new(varType);
+    }
+}
