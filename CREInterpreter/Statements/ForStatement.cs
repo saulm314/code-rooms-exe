243b8f73c003bfb170b9d9cb074c93cb5aaddab1
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-15 14:27:51 +0100

    Add ForStatement

---
+++ CREInterpreter/Statements/ForStatement.cs
@@ -0,0 +1,26 @@
+﻿using CREInterpreter.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class ForStatement(ReadOnlyMemory<char> chunkText, ReadOnlyMemory<IToken> tokens,
+    ReadOnlyMemory<IToken> intialiserStatementTokens, ReadOnlyMemory<IToken> expressionTokens, ReadOnlyMemory<IToken> iteratorStatementTokens)
+    : Statement(chunkText, tokens)
+{
+    public ReadOnlyMemory<IToken> InitialiserStatementTokens => intialiserStatementTokens;
+
+    public ReadOnlyMemory<IToken> ExpressionTokens => expressionTokens;
+
+    public ReadOnlyMemory<IToken> IteratorStatementTokens => iteratorStatementTokens;
+
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<StatementExecution> Execute(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+}
