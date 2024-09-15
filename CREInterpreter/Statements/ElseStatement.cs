ebcbd320f023a9cd87bc5fcb7f0cf4937b12b7a7
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-15 12:35:43 +0100

    Add ElseStatement

---
+++ CREInterpreter/Statements/ElseStatement.cs
@@ -0,0 +1,18 @@
+﻿using CREInterpreter.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class ElseStatement(ReadOnlyMemory<char> chunkText, ReadOnlyMemory<IToken> tokens) : Statement(chunkText, tokens)
+{
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<InterpreterException?> Run(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+}
