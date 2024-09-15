70b1173cfc00b828e30fa411576af0ac78fcce3f
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-15 12:36:57 +0100

    Add WhileStatement

---
+++ CREInterpreter/Statements/WhileStatement.cs
@@ -0,0 +1,21 @@
+﻿using CREInterpreter.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class WhileStatement(ReadOnlyMemory<char> chunkText, ReadOnlyMemory<IToken> tokens, ReadOnlyMemory<char> expressionTokens)
+    : Statement(chunkText, tokens)
+{
+    public ReadOnlyMemory<char> ExpressionTokens => expressionTokens;
+
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<InterpreterException?> Run(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+}
