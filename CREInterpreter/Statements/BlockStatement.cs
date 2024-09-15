397636b8f72e1855f4fcea6f5077e987049eadcc
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-15 14:38:32 +0100

    Add BlockStatement

---
+++ CREInterpreter/Statements/BlockStatement.cs
@@ -0,0 +1,21 @@
+﻿using CREInterpreter.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class BlockStatement(ReadOnlyMemory<char> chunkText, ReadOnlyMemory<IToken> tokens, ReadOnlyMemory<IToken> blockTokens)
+    : Statement(chunkText, tokens)
+{
+    public ReadOnlyMemory<IToken> BlockTokens => blockTokens;
+
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<StatementExecution> Execute(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+}
