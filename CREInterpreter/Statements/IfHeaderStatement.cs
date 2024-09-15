b44aa7b5b7c5b36f21201dc5c46d778fdcb9da7d
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-15 12:30:39 +0100

    Add IfHeaderStatement

---
+++ CREInterpreter/Statements/IfHeaderStatement.cs
@@ -0,0 +1,21 @@
+﻿using CREInterpreter.Tokens;
+using System;
+using System.Collections.Generic;
+
+namespace CREInterpreter.Statements;
+
+public class IfHeaderStatement(ReadOnlyMemory<char> chunkText, ReadOnlyMemory<IToken> tokens, ReadOnlyMemory<IToken> expressionTokens)
+    : Statement(chunkText, tokens)
+{
+    public ReadOnlyMemory<IToken> ExpressionTokens => expressionTokens;
+
+    public override InterpreterException? Compile(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override IEnumerable<InterpreterException?> Run(Memory memory)
+    {
+        throw new NotImplementedException();
+    }
+}
