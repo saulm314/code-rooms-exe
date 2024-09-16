5290ef656bd24fb9177d31d7fc86b7bd39dd8303
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-16 17:11:25 +0100

    Add DeclarationStatement to StatementSeparator

--- CREInterpreter/Statements/StatementSeparator.cs
+++ CREInterpreter/Statements/StatementSeparator.cs
@@ -23,6 +23,20 @@ public static class StatementSeparator
         return new EmptyStatement(text, tokens[index..++index]);
     }
 
+    private static IStatement? GetDeclarationStatement(ReadOnlyMemory<char> text, ReadOnlyMemory<IToken> tokens, ref int index)
+    {
+        ReadOnlySpan<IToken> tokenSpan = tokens.Span;
+        if (tokens.Length < index + 3)
+            return null;
+        if (tokenSpan[index] is not TypeNameToken typeNameToken)
+            return null;
+        if (tokenSpan[index + 1] is not VariableNameToken variableNameToken)
+            return null;
+        if (tokenSpan[index + 2] is not SemicolonSymbolToken)
+            return null;
+        return new DeclarationStatement(text, tokens[index..(index += 3)], typeNameToken._VarType, variableNameToken.Text);
+    }
+
     private static InvalidStatement GetInvalidStatement(ReadOnlyMemory<char> text, ReadOnlyMemory<IToken> tokens, ref int index)
     {
         return new(text, tokens[index..++index]);
