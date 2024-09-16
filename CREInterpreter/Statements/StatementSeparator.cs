2e3383b936563e6f18c29aa45a85bc090004df3d
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-16 21:36:59 +0100

    Add WriteVariableStatement to StatementSeparator

--- CREInterpreter/Statements/StatementSeparator.cs
+++ CREInterpreter/Statements/StatementSeparator.cs
@@ -44,26 +44,38 @@ public static class StatementSeparator
                 return null;
             if (tokenSpan[index + 2] is not EqualsSymbolToken)
                 return null;
-            int openTokenCount = 0;
-            for (int i = index + 3; i < tokens.Length; i++)
-            {
-                GetOpenTokenCount(tokenSpan[i], ref openTokenCount);
-                if (openTokenCount > 0)
-                    continue;
-                if (tokenSpan[i] is not SemicolonSymbolToken)
-                    continue;
-                index = i + 1;
-                return new DeclarationInitialisationStatement
-                (
-                    text,
-                    tokens[startIndex..index],
-                    typeNameToken._VarType,
-                    variableNameToken.Text,
-                    tokens[(startIndex + 3)..index]
-                );
-            }
-            return new InvalidStatement(text, tokens[startIndex..], new($"Declaration-initialisation statement not followed up by a semicolon"));
+            if (!SkipPastFirstSpecificToken<SemicolonSymbolToken>(tokenSpan, ref index))
+                return new InvalidStatement(text, tokens[startIndex..], new($"Declaration-initialisation statement not followed up by a semicolon"));
+            return new DeclarationInitialisationStatement
+            (
+                text,
+                tokens[startIndex..index],
+                typeNameToken._VarType,
+                variableNameToken.Text,
+                tokens[(startIndex + 3)..(index - 1)]
+            );
         }
+    }
+
+    private static IStatement? GetWriteVariableStatement(ReadOnlyMemory<char> text, ReadOnlyMemory<IToken> tokens, ref int index)
+    {
+        int startIndex = index;
+        ReadOnlySpan<IToken> tokenSpan = tokens.Span;
+        if (tokens.Length < index + 4)
+            return null;
+        if (tokenSpan[index] is not VariableNameToken variableNameToken)
+            return null;
+        if (tokenSpan[index + 1] is not EqualsSymbolToken)
+            return null;
+        if (!SkipPastFirstSpecificToken<SemicolonSymbolToken>(tokenSpan, ref index))
+            return new InvalidStatement(text, tokens[startIndex..], new($"Write-variable statement not followed up by a semicolon"));
+        return new WriteVariableStatement
+        (
+            text,
+            tokens[startIndex..index],
+            variableNameToken.Text,
+            tokens[(startIndex + 2)..(index - 1)]
+        );
     }
 
     private static InvalidStatement GetInvalidStatement(ReadOnlyMemory<char> text, ReadOnlyMemory<IToken> tokens, ref int index)
@@ -82,5 +94,22 @@ public static class StatementSeparator
                 openTokenCount--;
                 break;
         }
+    }
+
+    private static bool SkipPastFirstSpecificToken<TToken>(ReadOnlySpan<IToken> tokenSpan, ref int index) where TToken : IToken
+    {
+        int openTokenCount = 0;
+        for (int i = index; i < tokenSpan.Length; i++)
+        {
+            GetOpenTokenCount(tokenSpan[i], ref openTokenCount);
+            if (openTokenCount > 0)
+                continue;
+            if (tokenSpan[i] is not TToken)
+                continue;
+            index = i + 1;
+            return true;
+        }
+        index = tokenSpan.Length;
+        return false;
     }
 }
