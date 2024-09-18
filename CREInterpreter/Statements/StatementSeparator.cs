4fd746c2846c543fe3e0dc45f71f843268b3bfd6
Saulius Markevicius <smsmvicious@gmail.com>
2024-09-18 16:49:29 +0100

    Fix DeclarationStatement separation

--- CREInterpreter/Statements/StatementSeparator.cs
+++ CREInterpreter/Statements/StatementSeparator.cs
@@ -18,58 +18,47 @@ public static class StatementSeparator
 
     private static IStatement? GetEmptyStatement(ReadOnlyMemory<char> chunkText, ReadOnlyMemory<IToken> chunkTokens, ref int index)
     {
-        int startIndex = index;
         ReadOnlySpan<IToken> tokenSpan = chunkTokens.Span[index..];
         if (tokenSpan[0] is not SemicolonSymbolToken)
             return null;
-        index++;
-        return new EmptyStatement(chunkText, chunkTokens[startIndex..index]);
+        return new EmptyStatement(chunkText, chunkTokens[index..++index]);
     }
 
-    private static IStatement? GetDeclarationStatement(ReadOnlyMemory<char> text, ReadOnlyMemory<IToken> tokens, ref int index)
+    private static IStatement? GetDeclarationStatement(ReadOnlyMemory<char> chunkText, ReadOnlyMemory<IToken> chunkTokens, ref int index)
     {
         int startIndex = index;
-        int i = index;
-        ReadOnlySpan<IToken> tokenSpan = tokens.Span;
-        if (tokens.Length < startIndex + 3)
-            return null;
-        if (tokenSpan[i++] is not TypeNameToken typeNameToken)
-            return null;
-        bool isArray =
-            tokenSpan[i] is OpenSquareBraceSymbolToken &&
-            tokenSpan[i + 1] is CloseSquareBraceSymbolToken;
-        if (isArray)
+        ReadOnlySpan<IToken> tokenSpan = chunkTokens.Span[index..];
+        return tokenSpan switch
         {
-            i += 2;
-            if (tokens.Length < startIndex + 5)
-                return null;
-        }
-        VarType varType = isArray ? typeNameToken._VarType.Array! : typeNameToken._VarType;
-        if (tokenSpan[i++] is not VariableNameToken variableNameToken)
-            return null;
-        if (tokenSpan[i] is not SemicolonSymbolToken)
-            return GetDeclarationInitialisationStatement(tokenSpan, ref index);
-        i++;
-        index = i;
-        return new DeclarationStatement(text, tokens[startIndex..index], varType, variableNameToken.Text);
+            [TypeNameToken typeNameToken, VariableNameToken variableNameToken, SemicolonSymbolToken, ..] =>
+                new DeclarationStatement(chunkText, chunkTokens[index..(index += 3)], typeNameToken._VarType, variableNameToken.Text),
 
-        IStatement? GetDeclarationInitialisationStatement(ReadOnlySpan<IToken> tokenSpan, ref int index)
+            [TypeNameToken typeNameToken, OpenSquareBraceSymbolToken, CloseSquareBraceSymbolToken, VariableNameToken variableNameToken,
+                SemicolonSymbolToken, ..] =>
+                new DeclarationStatement(chunkText, chunkTokens[index..(index += 5)], typeNameToken._VarType.Array!, variableNameToken.Text),
+
+            [TypeNameToken typeNameToken, VariableNameToken variableNameToken, EqualsSymbolToken, ..] =>
+                GetDeclarationInitialisationStatement(tokenSpan, ref index, 3, typeNameToken._VarType, variableNameToken.Text),
+
+            [TypeNameToken typeNameToken, OpenSquareBraceSymbolToken, CloseSquareBraceSymbolToken, VariableNameToken variableNameToken,
+                EqualsSymbolToken, ..] =>
+                GetDeclarationInitialisationStatement(tokenSpan, ref index, 5, typeNameToken._VarType.Array!, variableNameToken.Text),
+
+            _ => null
+        };
+
+        IStatement? GetDeclarationInitialisationStatement(ReadOnlySpan<IToken> tokenSpan, ref int chunkIndex, int offset,
+            VarType varType, ReadOnlyMemory<char> variableName)
         {
-            if (tokens.Length < i + 2)
-                return null;
-            if (tokenSpan[i++] is not EqualsSymbolToken)
-                return null;
-            index = i;
-            if (!SkipPastFirstSpecificToken<SemicolonSymbolToken>(tokenSpan, ref index))
-                return new InvalidStatement(text, tokens[startIndex..], new($"Declaration-initialisation statement not followed up by a semicolon"));
-            return new DeclarationInitialisationStatement
-            (
-                text,
-                tokens[startIndex..index],
-                typeNameToken._VarType,
-                variableNameToken.Text,
-                tokens[(startIndex + 3)..(index - 1)]
-            );
+            SkipToFirstTopLevelSpecificToken<SemicolonSymbolToken>(tokenSpan, ref offset);
+            if (offset == -1)
+            {
+                chunkIndex = chunkTokens.Length;
+                return new InvalidStatement(chunkText, chunkTokens[chunkIndex..], new("Declaration-initialisation statement never followed by a semicolon"));
+            }
+            chunkIndex += offset + 1;
+            return new DeclarationInitialisationStatement(chunkText, chunkTokens[startIndex..chunkIndex], varType, variableName,
+                chunkTokens[(startIndex + 3)..(chunkIndex - 1)]);
         }
     }
 
@@ -83,8 +72,8 @@ public static class StatementSeparator
             return null;
         if (tokenSpan[index + 1] is not EqualsSymbolToken)
             return null;
-        if (!SkipPastFirstSpecificToken<SemicolonSymbolToken>(tokenSpan, ref index))
-            return new InvalidStatement(text, tokens[startIndex..], new($"Write-variable statement not followed up by a semicolon"));
+        //if (!SkipToFirstTopLevelSpecificToken<SemicolonSymbolToken>(tokenSpan, ref index))
+        //    return new InvalidStatement(text, tokens[startIndex..], new($"Write-variable statement not followed up by a semicolon"));
         return new WriteVariableStatement
         (
             text,
@@ -112,7 +101,16 @@ public static class StatementSeparator
         }
     }
 
-    private static bool SkipPastFirstSpecificToken<TToken>(ReadOnlySpan<IToken> tokenSpan, ref int index) where TToken : IToken
+    // Given a span, a starting index, and a token type,
+    // the function sets the index to correspond to the first element (at or after the starting index) with the specific token type,
+    // with the further criterion that the token must be top-level,
+    // i.e. it must not be inside any pairs of brackets/braces that opened at or after the starting index
+    // alternatively, if no such element exists, then index is set to -1
+    // e.g. if tokenSpan forms the text "for (int i = 0; i < 5; i = i + 1);", then if the starting index is 0 and TToken is SemicolonSymbolToken,
+    //      then the position of the final semicolon will be returned, since the first two semicolons are in parentheses
+    // on the other hand if the starting index is 2 (i.e. corresponding to "int"), then the first semicolon will be returned,
+    //      since the bracket was opened before the starting index
+    private static void SkipToFirstTopLevelSpecificToken<TToken>(ReadOnlySpan<IToken> tokenSpan, ref int index) where TToken : IToken
     {
         int openTokenCount = 0;
         for (int i = index; i < tokenSpan.Length; i++)
@@ -122,10 +120,9 @@ public static class StatementSeparator
                 continue;
             if (tokenSpan[i] is not TToken)
                 continue;
-            index = i + 1;
-            return true;
+            index = i;
+            return;
         }
-        index = tokenSpan.Length;
-        return false;
+        index = -1;
     }
 }
