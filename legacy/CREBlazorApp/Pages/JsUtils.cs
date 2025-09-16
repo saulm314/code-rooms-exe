615ca6bb464ec6db9d55eb6174cb8757ac2afc78
Saulius Markevicius <saulius0412@gmail.com>
2025-09-16 21:25:27 +0100

    Add auto scroll to bottom for stack

---
+++ legacy/CREBlazorApp/Pages/JsUtils.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.JSInterop;
+
+namespace CREBlazorApp.Pages;
+
+public static class JsUtils
+{
+    public static async Task ScrollToBottom(this IJSRuntime js, string elementId) => await js.InvokeVoidAsync("scrollToBottom", elementId);
+}
