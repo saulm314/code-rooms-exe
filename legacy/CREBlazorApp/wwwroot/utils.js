61cfa3f5dec93b8542c63560478f4bda586c6191
Saulius Markevicius <saulius0412@gmail.com>
2025-09-17 03:04:39 +0100

    Add multiple line shift+tab support

--- legacy/CREBlazorApp/wwwroot/utils.js
+++ legacy/CREBlazorApp/wwwroot/utils.js
@@ -5,6 +5,21 @@ function scrollToBottom(elementId) {
 
 function addTextEditorTabEventListener(textEditorId) {
     const textEditor = document.getElementById(textEditorId);
+
+    let isShiftPressed = false;
+
+    textEditor.addEventListener('keydown', function(event) {
+        if (event.code !== 'ShiftLeft')
+            return;
+        isShiftPressed = true;
+    });
+
+    textEditor.addEventListener('keyup', function(event) {
+        if (event.code !== 'ShiftLeft')
+            return;
+        isShiftPressed = false;
+    });
+
     textEditor.addEventListener('keydown', function(event) {
         if (event.key !== 'Tab')
             return;
@@ -12,16 +27,53 @@ function addTextEditorTabEventListener(textEditorId) {
         const start = this.selectionStart;
         const end = this.selectionEnd;
         let middle = this.value.substring(start, end);
+
         const initialLength = middle.length;
-        middle = middle.replaceAll('\n', '\n    ');
+        let spacesRemovedRight = 0;
+        let spacesRemovedLeft = 0;
+        let newStart = start;
+        if (!isShiftPressed)
+            middle = middle.replaceAll('\n', '\n    ');
+        else {
+            middle = middle.replaceAll('\n    ', '\n');
+            for (let i = 0; i < 4; i++) {
+                if (middle[i] !== ' ')
+                    break;
+                spacesRemovedRight++;
+            }
+            middle = middle.substring(spacesRemovedRight);
+        }
         const offset = middle.length - initialLength;
-        this.value = this.value.substring(0, start) + '    ' + middle + this.value.substring(end);
-        if (start === end) {
-            this.selectionStart = this.selectionEnd = start + 4;
+
+        if (!isShiftPressed)
+            this.value = this.value.substring(0, start) + '    ' + middle + this.value.substring(end);
+        else {
+            for (let i = start - 1; i >= start + spacesRemovedRight - 4 && i >= 0; i--) {
+                if (this.value[i] !== ' ')
+                    break;
+                newStart = i;
+            }
+            spacesRemovedLeft = start - newStart;
+            this.value = this.value.substring(0, newStart) + middle + this.value.substring(end);
+        }
+
+        if (!isShiftPressed) {
+            if (start === end) {
+                this.selectionStart = this.selectionEnd = start + 4;
+            }
+            else {
+                this.selectionStart = start;
+                this.selectionEnd = end + offset + 4;
+            }
         }
         else {
-            this.selectionStart = start;
-            this.selectionEnd = end + offset + 4;
+            if (start === end) {
+                this.selectionStart = this.selectionEnd = newStart;
+            }
+            else {
+                this.selectionStart = newStart;
+                this.selectionEnd = end + offset - spacesRemovedLeft;
+            }
         }
     });
 }
