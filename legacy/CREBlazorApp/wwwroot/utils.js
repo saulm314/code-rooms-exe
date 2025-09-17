25359cc0880613ed03d31110c191e61a55520daa
Saulius Markevicius <saulius0412@gmail.com>
2025-09-17 02:16:12 +0100

    Add multiple line tab support

--- legacy/CREBlazorApp/wwwroot/utils.js
+++ legacy/CREBlazorApp/wwwroot/utils.js
@@ -6,12 +6,22 @@ function scrollToBottom(elementId) {
 function addTextEditorTabEventListener(textEditorId) {
     const textEditor = document.getElementById(textEditorId);
     textEditor.addEventListener('keydown', function(event) {
-        if (event.key === 'Tab') {
-            event.preventDefault();
-            const start = this.selectionStart;
-            const end = this.selectionEnd;
-            this.value = this.value.substring(0, start) + '    ' + this.value.substring(end);
-            this.selectionstart = this.selectionEnd = start + 4;
+        if (event.key !== 'Tab')
+            return;
+        event.preventDefault();
+        const start = this.selectionStart;
+        const end = this.selectionEnd;
+        let middle = this.value.substring(start, end);
+        const initialLength = middle.length;
+        middle = middle.replaceAll('\n', '\n    ');
+        const offset = middle.length - initialLength;
+        this.value = this.value.substring(0, start) + '    ' + middle + this.value.substring(end);
+        if (start === end) {
+            this.selectionStart = this.selectionEnd = start + 4;
+        }
+        else {
+            this.selectionStart = start;
+            this.selectionEnd = end + offset + 4;
         }
     });
 }
