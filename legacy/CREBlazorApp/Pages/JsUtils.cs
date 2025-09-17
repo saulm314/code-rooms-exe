780a3ffdb75b030d36b5cbbd7f4e1f0a99e001b2
Saulius Markevicius <saulius0412@gmail.com>
2025-09-17 01:58:51 +0100

    Add text editor basic tab support

--- legacy/CREBlazorApp/Pages/JsUtils.cs
+++ legacy/CREBlazorApp/Pages/JsUtils.cs
@@ -5,4 +5,7 @@ namespace CREBlazorApp.Pages;
 public static class JsUtils
 {
     public static async Task ScrollToBottom(this IJSRuntime js, string elementId) => await js.InvokeVoidAsync("scrollToBottom", elementId);
+
+    public static async Task AddTextEditorTabEventListener(this IJSRuntime js, string textEditorId)
+        => await js.InvokeVoidAsync("addTextEditorTabEventListener", textEditorId);
 }
--- legacy/CREBlazorApp/wwwroot/utils.js
+++ legacy/CREBlazorApp/wwwroot/utils.js
@@ -2,3 +2,16 @@ function scrollToBottom(elementId) {
     const element = document.getElementById(elementId);
     element.scrollTop = element.scrollHeight;
 }
+
+function addTextEditorTabEventListener(textEditorId) {
+    const textEditor = document.getElementById(textEditorId);
+    textEditor.addEventListener('keydown', function(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            const start = this.selectionStart;
+            const end = this.selectionEnd;
+            this.value = this.value.substring(0, start) + '    ' + this.value.substring(end);
+            this.selectionstart = this.selectionEnd = start + 4;
+        }
+    });
+}
