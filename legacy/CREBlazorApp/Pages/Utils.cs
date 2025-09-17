0275f42f511319c5396ecfce5df4a1c8af6aee4a
Saulius Markevicius <saulius0412@gmail.com>
2025-09-17 23:23:55 +0100

    Add support for formatting for level 10 onwards

---
+++ legacy/CREBlazorApp/Pages/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace CREBlazorApp.Pages;
+
+public static class Utils
+{
+    public static string GetSpaces(int n)
+    {
+        char[] chars = new char[n];
+        Array.Fill(chars, ' ');
+        return new(chars);
+    }
+}
