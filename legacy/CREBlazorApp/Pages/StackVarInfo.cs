55151fa7e1dcdb11a1fad92ad856574bc253aa17
Saulius Markevicius <saulius0412@gmail.com>
2025-09-18 04:29:48 +0100

    Add variable font sizes for stack names

--- legacy/CREBlazorApp/Pages/StackVarInfo.cs
+++ legacy/CREBlazorApp/Pages/StackVarInfo.cs
@@ -1,3 +1,17 @@
 ﻿namespace CREBlazorApp.Pages;
 
-public readonly record struct StackVarInfo(string SeparatorColor, VarInfo PVarInfo, string Name);
+public readonly record struct StackVarInfo(string SeparatorColor, VarInfo PVarInfo, string Name, int FontSize)
+{
+    public static int GetFontSize(int length)
+        => length switch
+        {
+            <= 12 => 16,
+            <= 13 => 14,
+            <= 14 => 13,
+            <= 16 => 12,
+            <= 17 => 11,
+            <= 19 => 10,
+            <= 21 => 9,
+            _ => 8
+        };
+}
