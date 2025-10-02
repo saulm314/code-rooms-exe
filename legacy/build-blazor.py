f9b48595ed9e40244576574cc118f261590d080f
Saulius Markevicius <saulius0412@gmail.com>
2025-10-02 19:01:07 +0100

    Add AWS deployment support

--- legacy/build-blazor.py
+++ legacy/build-blazor.py
@@ -8,10 +8,12 @@ import sys
 PORT = 8080
 
 def get_env(args):
-    if len(args) >= 2 and args[1] == "deploy":
-        env = "deploy"
-    else:
-        env = "local"
+    env = "local"
+    if len(args) >= 2:
+        if args[1] == "deploy":
+            env = "deploy"
+        elif args[1] == "deploy-aws":
+            env = "deploy-aws"
     print(f"environment: {env}")
     return env
 
@@ -79,16 +81,21 @@ public static class GeneratedLevels
                 generated_levels_cs.write(solution.read())
             generated_levels_cs.write('\n""",\n')
         generated_levels_cs.write("""    ];
-}""")  
+}""")
 
 def build(env):
+    if env == "deploy-aws":
+        dotnet_path = "./dotnet9/dotnet"
+    else:
+        dotnet_path = "dotnet"
+
     if env == "local":
         # in a live deployment, we don't need to remove these as we are already in a fresh environment
         print("Removing existing published data...")
         shutil.rmtree(path("code-rooms-exe"), ignore_errors=True)
     
     print("Publishing CREBlazorApp...")
-    process_resp = subprocess.run(["dotnet", "publish", path("CREBlazorApp"), "-c", "CREBlazor", "-o", path("publish")])
+    process_resp = subprocess.run([dotnet_path, "publish", path("CREBlazorApp"), "-c", "CREBlazor", "-o", path("publish")])
     if process_resp.returncode != 0:
         print("Dotnet build failed")
         return False
